cb84f19a37d2f4e3ca25101473994177814f4483
Sibeesh Venu <venu@medialesson.de>
2020-07-18 19:46:02 +0200

    change to stream

--- GetLatestPosts.cs
+++ GetLatestPosts.cs
@@ -10,31 +10,42 @@ using System.ServiceModel.Syndication;
 using System.Xml;
 using GitHubFuncs.ExtensionMethods;
 using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubFuncs
 {
     public static class GetLatestPosts
     {
         [FunctionName("GetLatestPosts")]
-        public static string Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request, ILogger log)
+        public static async Task<FileStreamResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request,
+            ExecutionContext context, ILogger log)
         {
             try
             {
-                var postImage = WriteOnImage(GetTopPost());
-                return postImage;
+                // var imgPath = Path.Combine(context.FunctionDirectory, "..\\images\\wordart.png");
+
+                // using var imgStream = new FileStream(imgPath, FileMode.Open, FileAccess.ReadWrite);
+                log.LogInformation("Returning stream now!");
+                return new FileStreamResult(await WriteOnImage(GetTopPost()), "image/png");
             }
             catch (System.Exception ex)
             {
+                log.LogError($"Something went wrong: {ex}");
                 throw ex;
             }
         }
 
-        private static string WriteOnImage(SyndicationItem feedItem)
+        private static async Task<Stream> WriteOnImage(SyndicationItem feedItem)
         {
-            using var img = Image.Load("images/canvas.jpg");
-            var font = SystemFonts.CreateFont("Arial", 20);
-            using var img2 = img.Clone(ctx => ctx.ApplyScalingWaterMark(font, feedItem.Summary.Text, Color.White, 5, true));
-            return img2.ToBase64String(PngFormat.Instance);
+            using var stream = new MemoryStream();
+            using var img = new Image<Rgba32>(500, 200);
+            var font = SystemFonts.CreateFont("Arial", 18);
+            img.Mutate(ctx => ctx.ApplyScalingWaterMark(font, feedItem.Summary.Text, Color.Black, 5, true));
+            await img.SaveAsync(stream, new PngEncoder());
+            return stream;
         }
 
         public static SyndicationItem GetTopPost()
