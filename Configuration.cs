aeacb3e1ddd784ac31e42148c5199866748f744d
Sibeesh Venu <venu@medialesson.de>
2020-07-20 17:40:11 +0200

    refactor

---
+++ Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace GitHubFuncs
+{
+    public static class Configuration
+    {
+        public static string BlogLink { get; set; } = "https://sibeeshpassion.com/feed";
+        public static int ImageWidth { get; set; } = 850;
+        public static int ImageHeight { get; set; } = 200;
+        public static string ContentType { get; set; } = "image/png";
+        public static string Font { get; set; } = "Arial";
+        public static int FontSize { get; set; } = 18;
+    }
+}
--- GetLatestPosts.cs
+++ GetLatestPosts.cs
@@ -1,19 +1,20 @@
+using GitHubFuncs.ExtensionMethods;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Xml;
-using GitHubFuncs.ExtensionMethods;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.PixelFormats;
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace GitHubFuncs
 {
@@ -28,7 +29,7 @@ namespace GitHubFuncs
                 // Had to do this, as it was throwing error "The input is not a valid Base-64 string as it contains a non-base 64 character"
                 string convert = baseString.Replace("data:image/png;base64,", String.Empty);
                 var bytes = Convert.FromBase64String(convert);
-                var result = new FileStreamResult(new MemoryStream(bytes), "image/png");
+                var result = new FileStreamResult(new MemoryStream(bytes), Configuration.ContentType);
                 log.LogInformation("Returning stream now!");
                 request.HttpContext.Response.Headers.Add("Cache-Control", "s-maxage=1, stale-while-revalidate");
                 return result; ;
@@ -40,21 +41,22 @@ namespace GitHubFuncs
             }
         }
 
-        private static string WriteOnImage(SyndicationItem feedItem)
+        private static string WriteOnImage(IEnumerable<SyndicationItem> feedItems)
         {
+            var titles = string.Join(", ", feedItems.Select(s => s.Title.Text).ToList());
             using var stream = new MemoryStream();
-            using var img = new Image<Rgba32>(850, 200);
-            var font = SystemFonts.CreateFont("Arial", 18);
-            img.Mutate(ctx => ctx.ApplyScalingWaterMark(font, feedItem.Summary.Text, Color.Black, 5, true));
+            using var img = new Image<Rgba32>(Configuration.ImageWidth, Configuration.ImageHeight);
+            var font = SystemFonts.CreateFont(Configuration.Font, Configuration.FontSize);
+            img.Mutate(ctx => ctx.ApplyScalingWaterMark(font, titles, Color.Black, 5, true));
             return img.ToBase64String(PngFormat.Instance);
         }
 
-        public static SyndicationItem GetTopPost()
+        public static IEnumerable<SyndicationItem> GetTopPost()
         {
-            var reader = XmlReader.Create("https://sibeeshpassion.com/feed");
+            var reader = XmlReader.Create(Configuration.BlogLink);
             var feed = SyndicationFeed.Load(reader);
             reader.Close();
-            return feed.Items.FirstOrDefault();
+            return feed.Items.Take(5);
         }
     }
 }
