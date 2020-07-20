aa25c2c859b8a93606af30839dfbffa5fe6b359e
Sibeesh Venu <venu@medialesson.de>
2020-07-20 17:57:18 +0200

    change the image height to 100

--- Configuration.cs
+++ Configuration.cs
@@ -4,7 +4,7 @@
     {
         public static string BlogLink { get; set; } = "https://sibeeshpassion.com/feed";
         public static int ImageWidth { get; set; } = 850;
-        public static int ImageHeight { get; set; } = 200;
+        public static int ImageHeight { get; set; } = 100;
         public static string ContentType { get; set; } = "image/png";
         public static string Font { get; set; } = "Arial";
         public static int FontSize { get; set; } = 5;
