2ecf3f7676c50e6688238119937f07508eb39e9f
Sibeesh Venu <venu@medialesson.de>
2020-08-19 11:53:45 +0200

    Generate and upload the image to blob

--- GetLatestPosts.cs
+++ GetLatestPosts.cs
@@ -1,9 +1,8 @@
 using GitHubFuncs.ExtensionMethods;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
@@ -11,34 +10,58 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
 using System.Xml;
 
 namespace GitHubFuncs
 {
-    public static class GetLatestPosts
+    public class GetLatestPosts
     {
+        private static ILogger _logger;
+        private const string _fileName = "latestpost.png";
+        private const string _blobContainerName = "github";
         [FunctionName("GetLatestPosts")]
-        public static FileStreamResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request, ILogger log)
+        public async Task Run([TimerTrigger("0 0 */1 * * *")] TimerInfo myTimer, ILogger log)
         {
             try
             {
-                var baseString = WriteOnImage(GetLatestFeeds());
-                // Had to do this, as it was throwing error "The input is not a valid Base-64 string as it contains a non-base 64 character"
-                string convert = baseString.Replace("data:image/png;base64,", String.Empty);
-                var bytes = Convert.FromBase64String(convert);
-                var result = new FileStreamResult(new MemoryStream(bytes), Configuration.ContentType);
-                log.LogInformation("Returning stream now!");
-                request.HttpContext.Response.Headers.Add("Cache-Control", "s-maxage=1, stale-while-revalidate");
-                return result; ;
+                _logger = log;
+                await UplaodImageToStorage();
             }
             catch (System.Exception ex)
             {
                 log.LogError($"Something went wrong: {ex}");
                 throw ex;
             }
+        }
+
+        private static async Task<bool> UplaodImageToStorage()
+        {
+            try
+            {
+                var baseString = WriteOnImage(GetLatestFeeds());
+                string convert = baseString.Replace("data:image/png;base64,", String.Empty);
+                var bytes = Convert.FromBase64String(convert);
+                if (CloudStorageAccount.TryParse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"), out CloudStorageAccount cloudStorageAccount))
+                {
+                    var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                    var cloudBlobContainer = cloudBlobClient.GetContainerReference(_blobContainerName);
+                    var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(_fileName);
+                    await cloudBlockBlob.UploadFromByteArrayAsync(bytes, 0, bytes.Length);
+                    _logger.LogInformation("Uploaded new image");
+                }
+                else
+                {
+                    _logger.LogError("Error in connection");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return false;
         }
 
         private static string WriteOnImage(IEnumerable<SyndicationItem> feedItems)
